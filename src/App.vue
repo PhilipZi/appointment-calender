4cd4933f83bd5db1b5c40103bb8006994be08c74
Philip Zimmermann <philip.zimmermann@freenet.de>
2023-04-19 15:04:05 +0200

    add bootstrap package

--- src/App.vue
+++ src/App.vue
@@ -125,9 +125,6 @@ export default {
 </script>
 
 <style>
-/* @import "~bootstrap/dist/css/bootstrap.min.css"; */
-/* @import "~@fortawesome/fontawesome-free/css/all.min.css"; */
-
 .square {
   width: 40px;
   height: 40px;
