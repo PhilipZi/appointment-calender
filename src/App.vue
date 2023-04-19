f7521f035875f6678996952320ac6d1438a27c75
Philip Zimmermann <philip.zimmermann@freenet.de>
2023-04-19 14:37:20 +0200

    add packages updates

--- src/App.vue
+++ src/App.vue
@@ -125,8 +125,8 @@ export default {
 </script>
 
 <style>
-@import "~bootstrap/dist/css/bootstrap.min.css";
-@import "~@fortawesome/fontawesome-free/css/all.min.css";
+/* @import "~bootstrap/dist/css/bootstrap.min.css"; */
+/* @import "~@fortawesome/fontawesome-free/css/all.min.css"; */
 
 .square {
   width: 40px;
