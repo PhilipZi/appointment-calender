a67ed20f729f0ba4c142049d452a364ce7976a90
Philip Zimmermann <philip.zimmermann@freenet.de>
2023-04-19 17:34:43 +0200

    out source components

--- src/App.vue
+++ src/App.vue
@@ -3,47 +3,11 @@
     <div class="row">
       <div class="col-12">
         <CalenderWeek />
-
-        <!-- Ende: Template für die Calendar-Week-Component -->
       </div>
     </div>
     <div class="row mt-3">
       <div class="col-4 offset-4">
-        <!-- Anfang: Template für die Calendar-Entry-Component -->
-        <div id="calender-entry">
-          <div class="card">
-            <div class="card-header text-center">
-              <h5>New Appointment For: <strong>Monday</strong></h5>
-            </div>
-            <div class="card-body">
-              <input
-                type="text"
-                class="form-control"
-                placeholder="Neuer Eintrag"
-              />
-              <select class="form-select mt-2">
-                <option value="-1">High</option>
-                <option value="0">Medium</option>
-                <option value="1">Low</option>
-              </select>
-              <div class="text-center mt-3">
-                <span
-                  v-for="i in 5"
-                  :key="i"
-                  class="d-inline-block alert alert-primary m-0 me-2 square"
-                  role="button"
-                >
-                </span>
-              </div>
-              <hr />
-              <div class="d-grid gap-2">
-                <button class="btn btn-primary">Submit</button>
-                <button class="btn btn-danger">Delete Content</button>
-              </div>
-            </div>
-          </div>
-        </div>
-        <!-- Ende: Template für die Calendar-Day-Component -->
+        <CalenderEntry />
       </div>
       <div class="col-2 offset-2">
         <div class="float-end">
@@ -52,37 +16,8 @@
             <i class="fas fa-cogs"></i>
           </button>
         </div>
-        <!-- Anfang: Template für die Calendar-Settings-Component -->
-        <div id="calendar-settings">
-          <div class="card">
-            <div class="card-header text-center bg-success text-white">
-              <strong>Settings</strong>
-            </div>
-            <div class="card-body">
-              <ul class="nav nav-pills nav-fill">
-                <li v-for="i in 2" :key="i" class="nav-item" role="button">
-                  <a class="nav-link"
-                    ><i class="fas fa-icons text-success"></i
-                  ></a>
-                </li>
-              </ul>
-              <hr />
-              <ul class="nav nav-pills nav-fill">
-                <li class="nav-item" role="button">
-                  <a class="nav-link"
-                    ><i class="fas fa-sort-numeric-down-alt text-success"></i
-                  ></a>
-                </li>
-                <li class="nav-item" role="button">
-                  <a class="nav-link"
-                    ><i class="fas fa-sort-alpha-down text-success"></i
-                  ></a>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </div>
-        <!-- Ende: Template für die Calendar-Day-Component -->
+
+        <CalenderSettings />
       </div>
     </div>
   </div>
@@ -90,11 +25,15 @@
 
 <script>
 import CalenderWeek from "./components/CalenderWeek.vue";
+import CalenderEntry from "./components/CalenderEntry.vue";
+import CalenderSettings from "./components/CalenderSettings.vue";
 
 export default {
   name: "App",
   components: {
     CalenderWeek,
+    CalenderEntry,
+    CalenderSettings,
   },
 };
 </script>
---
+++ src/components/CalenderEntry.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="calender-entry">
+    <div class="card">
+      <div class="card-header text-center">
+        <h5>New Appointment For: <strong>Monday</strong></h5>
+      </div>
+      <div class="card-body">
+        <input type="text" class="form-control" placeholder="Neuer Eintrag" />
+        <select class="form-select mt-2">
+          <option value="-1">High</option>
+          <option value="0">Medium</option>
+          <option value="1">Low</option>
+        </select>
+        <div class="text-center mt-3">
+          <span
+            v-for="i in 5"
+            :key="i"
+            class="d-inline-block alert alert-primary m-0 me-2 square"
+            role="button"
+          >
+          </span>
+        </div>
+        <hr />
+        <div class="d-grid gap-2">
+          <button class="btn btn-primary">Submit</button>
+          <button class="btn btn-danger">Delete Content</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CalenderEntry",
+};
+</script>
+
+<style scoped></style>
